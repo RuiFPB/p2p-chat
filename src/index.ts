15b0c0e0f7f395a2d69b0ee71eff94744b3cd74c
Rui Barbosa <1211106@isep.ipp.pt>
2025-05-26 18:18:02 +0100

    Implemented the chat app without any encryption.
    Had to change the tsconfig to use es2017
    Using PeerJS for P2P connection. Will have to deploy peerjs server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,75 @@
-console.log('Hello Vite build');
+import Peer, {DataConnection} from "peerjs";
+
+let log : HTMLTextAreaElement = document.getElementById("messagelog") as HTMLTextAreaElement;
+let userName : string = `user${String(Math.floor(Math.random() * 10000)).padStart(4, "0")}u`
+
+let peer = new Peer(userName);
+let conn: DataConnection;
+let ready: boolean = false;
+
+peer.on('open', (id) => {
+    userName = id;
+    logMessage("Your username is '" + userName + "'");
+})
+
+peer.on('connection', (socket) => {
+    conn = socket;
+    logMessage("Connected from " + socket.peer);
+    conn.on('data', dataHandler)
+    ready = true;
+})
+
+function dataHandler(d : unknown) {
+    let data = JSON.parse(d as string)
+    logMessage(data.message, data.username);
+}
+
+function logMessage(message:string, from : string = "system") {
+    log.textContent += `<${from}>: ${message}\n`;
+    log.scrollTop = log.scrollHeight;
+}
+
+function clearLog() {
+    log.textContent = "";
+}
+
+function changeUsername() {
+    let newUsername = "";
+    do {
+        newUsername = prompt("Enter your username") as string;
+    } while (newUsername.length < 0 || newUsername == "system" || newUsername == "System");
+
+    userName = newUsername;
+    logMessage(`Username changed to '${newUsername}'`);
+}
+
+function connectTo() {
+    let ip = prompt("Enter peer id") as string;
+
+    conn = peer.connect(ip);
+
+    conn.on('open', () => {
+        ready = true;
+        logMessage("Connected to " + ip);
+    })
+    conn.on('data', dataHandler)
+}
+function sendMessage() {
+    let message = (document.getElementById("text") as HTMLInputElement).value as string;
+
+
+    if (ready) {
+        console.log(JSON.stringify({ username: userName, message: message}))
+        conn.send(JSON.stringify({ username: userName, message: message}));
+        logMessage(message, userName);
+    }
+}
+
+document.getElementById("changeuser")?.addEventListener("click", () => {alert("disabled")});
+document.getElementById("clearmessages")?.addEventListener("click", clearLog);
+document.getElementById("connect")?.addEventListener("click", connectTo);
+document.getElementById("sendmessage")?.addEventListener("click", sendMessage);
+
+
+logMessage("System start!");
+logMessage("Please, establish a connection before sending messages");
